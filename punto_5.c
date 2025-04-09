cd9b29fb0fa5b0da87af0cbbfd0437a7e665032b
ManuelCoggiola <manuelcm.xd@gmail.com>
2025-04-09 15:41:53 -0300

    hola

---
+++ punto_5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+void concatenarCadenas(char *arr1, char *arr2){
+	 char *inicio=arr1;
+	 while (*arr1 != '\0') {
+        if (*arr1 == '\n') {
+            *arr1 = '\0';
+            break;
+        }
+		arr1++;
+		}
+		
+	strcat(inicio, arr2);
+	printf("La cadena concatenada resulta siendo: %s", inicio);
+}
+int main(){
+	char cadena1[1000], cadena2[500];
+	printf("Ingrese la primer cadena: ");
+	fgets(cadena1, sizeof(cadena1), stdin);
+	
+	printf("Ingrese la segunda cadena: ");
+	fgets(cadena2, sizeof(cadena2), stdin);
+	
+	concatenarCadenas(cadena1, cadena2);
+}
